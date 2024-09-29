6ee77820d87c258f102a0449012767a186b11083
Roman Kiryuhin <roman.kiryuhin@ru.axept.com>
2024-09-29 20:10:35 +0300

    Fix DetailVC

--- AstroSoft/AstroSoft/Controllers/DetailViewController/DetailViewController.swift
+++ AstroSoft/AstroSoft/Controllers/DetailViewController/DetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+final class DetailViewController: UIViewController {
     // MARK: - UI
     private var collectionView: UICollectionView!
     private var refControl: UIRefreshControl!
